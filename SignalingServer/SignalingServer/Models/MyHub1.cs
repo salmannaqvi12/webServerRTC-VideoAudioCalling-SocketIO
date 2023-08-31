818ba9f690fb165d6af8c8fdd0188bd9185d9af3
Salman Naqvi <syedsalman637@outlook.com>
2023-09-01 03:11:36 +0500

    Complete Web Server RTC with Socket IO

---
+++ SignalingServer/SignalingServer/Models/MyHub1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication5.Models
+{
+    public class WebRtcHub : Hub
+    {
+        public void Send(string message , string type)
+        {
+            Clients.Others.newMessage(message , type);
+            //Clients.All.addNewMessageToPage(message);
+
+        }
+    }
+}
